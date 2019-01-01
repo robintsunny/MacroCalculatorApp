7bc84831691479d9b9d3e57da8ffce82394d648a
robin <robintsunny@gmail.com>
2018-12-31 16:29:31 -0800

    Create Food Items

--- macro.js
+++ macro.js
@@ -12,51 +12,63 @@ class Food {
     }
 }
 
-class Goals {
-    constructor(fat,carb,pro,fib) {
-        this.fat = fat,
-        this.carb = carb,
-        this.pro = pro, 
-        this.fib = fib
-    }
-}
-
 
 // set goals
 const setGoals = () => {
+    //comes in as strings
     const fat = document.querySelector(".goals_f").value;
     const carb = document.querySelector(".goals_c").value;
     const pro = document.querySelector(".goals_p").value;
 
-    console.log([fat,carb,pro])
+    // convert string to int
     const fatCals = 9 * parseInt(fat)
     const carbCals = 4 * parseInt(carb)
     const proCals = 4 * parseInt(pro)
     const totalCals =  fatCals + carbCals + proCals;
 
+    // calories as percentage
     const fatPerc = (fatCals/totalCals*100).toFixed(2);
     const carbPerc = (carbCals/totalCals*100).toFixed(2);
     const proPerc = (proCals/totalCals*100).toFixed(2);
 
+    // render to UI
     document.querySelector(".curr_f").textContent = `Fat: ${fat} grams, ${fatPerc}%`;
     document.querySelector(".curr_c").textContent = `Carb: ${carb} grams, ${carbPerc}%`;
     document.querySelector(".curr_p").textContent = `Protein: ${pro} grams, ${proPerc}%`;
     document.querySelector(".curr_cal").textContent = `Total Calories: ${totalCals}`;
-
-
 }
 
 document.querySelector('.set_goal').addEventListener('click',setGoals)
 
 
-// edit goals
-
 
 
 
 // create food item and save
+const favorites = [];
 
-// edit food item
+const createFood = () => {
+    const name = document.querySelector(".new_name").value;
+    const fat = parseInt(document.querySelector(".new_f").value);
+    const carb = parseInt(document.querySelector(".new_c").value);
+    const pro = parseInt(document.querySelector(".new_p").value);
+    const fib = parseInt(document.querySelector(".new_fib").value);
+
+    totalCarb = carb-fib; //account for fiber
+    
+    const food = new Food(name, fat, totalCarb, pro, fib);
+
+
+    favorites.push(food);
+    updateDropdown(favorites);
+}
+
+document.querySelector('.create_food').addEventListener('click', createFood)
+
+
+
+
+// update dropdown options
 
 
 
